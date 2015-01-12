3c917705c76e0ee5c9cda57a87a402cf5b64fd6c
Allen Liu <kamyu104@gmail.com>
2015-01-12 22:33:28 +0800

    update

---
+++ MySQL/combine-two-tables.sql
@@ -0,0 +1,34 @@
+# Time:  O(n), n is size of Person Table
+# Space: O(n)
+#
+# Table: Person
+#
+# +-------------+---------+
+# | Column Name | Type    |
+# +-------------+---------+
+# | PersonId    | int     |
+# | FirstName   | varchar |
+# | LastName    | varchar |
+# +-------------+---------+
+# PersonId is the primary key column for this table.
+# Table: Address
+#
+# +-------------+---------+
+# | Column Name | Type    |
+# +-------------+---------+
+# | AddressId   | int     |
+# | PersonId    | int     |
+# | City        | varchar |
+# | State       | varchar |
+# +-------------+---------+
+# AddressId is the primary key column for this table.
+#
+# Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
+#
+#FirstName, LastName, City, State
+#
+#
+# Write your MySQL query statement below
+SELECT FirstName, LastName, City, State FROM Person LEFT JOIN Address
+       ON Person.PersonId=Address.PersonId
+
---
+++ MySQL/nth-highest-salary.sql
@@ -0,0 +1,26 @@
+# Time:  O(n^2)
+# Space: O(n)
+#
+# Write a SQL query to get the nth highest salary from the Employee table.
+#
+# +----+--------+
+# | Id | Salary |
+# +----+--------+
+# | 1  | 100    |
+# | 2  | 200    |
+# | 3  | 300    |
+# +----+--------+
+# For example, given the above Employee table, the nth highest salary where n = 2 is 200. If there is no nth highest salary, then the query should return null.
+#
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+     # Write your MySQL query statement below.
+     SELECT MAX(Salary) /*This is the outer query part */
+            FROM Employee Emp1
+            WHERE (N-1) = ( /* Subquery starts here */
+                 SELECT COUNT(DISTINCT(Emp2.Salary))
+                        FROM Employee Emp2
+                        WHERE Emp2.Salary > Emp1.Salary)
+                 );
+END
