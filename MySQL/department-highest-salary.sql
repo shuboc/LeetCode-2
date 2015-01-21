34052c1ffd070951e71c09ad8a6bed3ed3903e70
kamyu <kamyu104@gmail.com>
2015-01-21 22:59:54 +0800

    Update department-highest-salary.sql

--- MySQL/department-highest-salary.sql
+++ MySQL/department-highest-salary.sql
@@ -31,7 +31,7 @@
 # Write your MySQL query statement below
 SELECT d.Department AS Department, e.Name AS Employee, d.Salary AS Salary
 FROM (SELECT Department.Id AS DepartmentId, Department.Name AS Department, emp.Salary AS Salary
-        FROM Department JOIN (SELECT DepartmentId, MAX(Salary) AS Salary FROM Employee GROUP BY Employee.DepartmentId) emp
+        FROM Department JOIN (SELECT DepartmentId, MAX(Salary) AS Salary FROM Employee GROUP BY DepartmentId) emp
         ON Department.Id = emp.DepartmentId) d 
       JOIN Employee e 
       ON e.DepartmentId = d.DepartmentId and e.Salary = d.Salary
