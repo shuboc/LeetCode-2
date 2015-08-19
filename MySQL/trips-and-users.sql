6b2626a9ccf1745cbf883c9397aa5ebc14b6a2a6
kamyu <kamyu104@users.noreply.github.com>
2015-08-19 10:27:33 +0800

    Update trips-and-users.sql

--- MySQL/trips-and-users.sql
+++ MySQL/trips-and-users.sql
@@ -51,6 +51,6 @@ t.Request_at Day,
 round(sum(case when t.Status = 'completed' then 0 else 1 end) / count(*), 2) Rate
 from Trips t
 inner join Users u 
-on t.Client_Id = u.Users_Id and u.Banned='No'
+on t.Client_Id = u.Users_Id and u.Banned = 'No'
 where t.Request_at between '2013-10-01' and '2013-10-03'
 group by t.Request_at
