e9c31a00a64b71abed524fdb3678aea7ff323dad
JinkeCao <jcao2014@outlook.com>
2016-07-31 16:15:07 +0800

    modified:   MySQL/consecutive-numbers.sql

--- MySQL/consecutive-numbers.sql
+++ MySQL/consecutive-numbers.sql
@@ -29,7 +29,7 @@ FROM (
 ) sq
 WHERE how_many_cnt_in_a_row >= 3
 
-# Solution 1
+# Solution 2
 SELECT DISTINCT l1.Num  as ConsecutiveNums
 FROM Logs l1, Logs l2, Logs l3  
 WHERE l1.Id + 1 = l2.Id AND l2.Id + 1 = l3.Id AND l1.Num = l2.Num AND l2.Num = l3.Num 
